7c0fc580e4967fc46aa060fec70ed24005207c9f
Andrew Chepurny <chip@prj-3.com>
2023-08-04 23:40:30 -0400

    Update lib includes for Google Python Client

--- GSCtoAAexporter.py
+++ GSCtoAAexporter.py
@@ -5,10 +5,10 @@ import jwt
 import re
 import httplib2
 import json
-import apiclient
+import googleapiclient
 
-from apiclient.discovery import build
-from apiclient.errors import HttpError
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.file import Storage
 from oauth2client.tools import argparser, run_flow
